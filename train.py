a10aeccf82668e8758a8eb4b6b28d03625fe6716
Wugengxian <60342704+Wugengxian@users.noreply.github.com>
2021-12-28 16:16:27 +0800

    first version

--- train.py
+++ train.py
@@ -1,90 +1,90 @@
 import time
-from options.train_options import TrainOptions
-from models import create_model
-from util.visualizer import Visualizer
+import argparse
+
+from torch import utils
+from datasets import imgDataset
 
 import torch
+import torch.nn as nn
+import torch.functional as F
 import torchvision
 import torchvision.transforms as transforms
+from models import ECCV6
+from util import processdata
+import tensorboard
 
-from util import util
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train a GAN based model")
+    parser.add_argument("-d",
+                        "--training_dir",
+                        type=str,
+                        required=True,
+                        help="Training directory (folder contains all 256*256 images)")
+    parser.add_argument("-t",
+                        "--test_image",
+                        type=str,
+                        default=None,
+                        help="Test image location")
+    parser.add_argument("-c",
+                        "--checkpoint_location",
+                        type=str,
+                        required=True,
+                        help="Place to save checkpoints")
+    parser.add_argument("-e",
+                        "--epoch",
+                        type=int,
+                        default=120,
+                        help="Epoches to run training")
+    parser.add_argument("-b",
+                        "--batch_size",
+                        type=int,
+                        default=20,
+                        help="batch size")
+    parser.add_argument("-w",
+                        "--num_workers",
+                        type=int,
+                        default=6,
+                        help="Number of workers to fetch data")
+    parser.add_argument("-p",
+                        "--pixel_loss_weights",
+                        type=float,
+                        default=1000.0,
+                        help="Pixel-wise loss weights")
+    parser.add_argument("--lr",
+                        type=float,
+                        default=1e-4,
+                        help="learning rate")
+    parser.add_argument("-i",
+                        "--checkpoint_every",
+                        type=int,
+                        default=100,
+                        help="Save checkpoint every k iteration (checkpoints for same epoch will overwrite)")
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == '__main__':
-    opt = TrainOptions().parse()
-
-    opt.dataroot = './dataset/ilsvrc2012/%s/' % opt.phase
-    dataset = torchvision.datasets.ImageFolder(opt.dataroot,
-                                               transform=transforms.Compose([
-                                                   transforms.RandomChoice([transforms.Resize(opt.loadSize, interpolation=1),
-                                                                            transforms.Resize(opt.loadSize, interpolation=2),
-                                                                            transforms.Resize(opt.loadSize, interpolation=3),
-                                                                            transforms.Resize((opt.loadSize, opt.loadSize), interpolation=1),
-                                                                            transforms.Resize((opt.loadSize, opt.loadSize), interpolation=2),
-                                                                            transforms.Resize((opt.loadSize, opt.loadSize), interpolation=3)]),
-                                                   transforms.RandomChoice([transforms.RandomResizedCrop(opt.fineSize, interpolation=1),
-                                                                            transforms.RandomResizedCrop(opt.fineSize, interpolation=2),
-                                                                            transforms.RandomResizedCrop(opt.fineSize, interpolation=3)]),
-                                                   transforms.RandomHorizontalFlip(),
-                                                   transforms.ToTensor()]))
-    dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, shuffle=True, num_workers=int(opt.num_threads))
+    opt = parse_args()
+    dataset = imgDataset(opt.training_dir)
+    dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, shuffle=True, num_workers=int(opt.num_workers))
 
     dataset_size = len(dataset)
     print('#training images = %d' % dataset_size)
 
-    model = create_model(opt)
-    model.setup(opt)
-    model.print_networks(True)
+    model = ECCV6.eccv16()
+    model = model.cuda()
+    model.train()
 
-    visualizer = Visualizer(opt)
-    total_steps = 0
+    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+    loss_fuction = nn.MSELoss()
 
-    for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay):
-        epoch_start_time = time.time()
-        iter_data_time = time.time()
-        epoch_iter = 0
-
+    for epoch in range(opt.epoch):
         # for i, data in enumerate(dataset):
-        for i, data_raw in enumerate(dataset_loader):
-            data_raw[0] = data_raw[0].cuda()
-            data = util.get_colorization_data(data_raw, opt, p=opt.sample_p)
-            if(data is None):
-                continue
-
-            iter_start_time = time.time()
-            if total_steps % opt.print_freq == 0:
-                # time to load data
-                t_data = iter_start_time - iter_data_time
-            visualizer.reset()
-            total_steps += opt.batch_size
-            epoch_iter += opt.batch_size
-            model.set_input(data)
-            model.optimize_parameters()
-
-            if total_steps % opt.display_freq == 0:
-                save_result = total_steps % opt.update_html_freq == 0
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
-
-            if total_steps % opt.print_freq == 0:
-                losses = model.get_current_losses()
-                # time to do forward&backward
-                t = time.time() - iter_start_time
-                visualizer.print_current_losses(epoch, epoch_iter, losses, t, t_data)
-                if opt.display_id > 0:
-                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, opt, losses)
-
-            if total_steps % opt.save_latest_freq == 0:
-                print('saving the latest model (epoch %d, total_steps %d)' %
-                      (epoch, total_steps))
-                model.save_networks('latest')
-
-            iter_data_time = time.time()
-
-        if epoch % opt.save_epoch_freq == 0:
-            print('saving the model at the end of epoch %d, iters %d' %
-                  (epoch, total_steps))
-            model.save_networks('latest')
-            model.save_networks(epoch)
-
-        print('End of epoch %d / %d \t Time Taken: %d sec' %
-              (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
-        model.update_learning_rate()
+        for i, (tens_orig, tens_rs_l, tens_rs_ab) in enumerate(dataset_loader):
+            optimizer.zero_grad()
+            tens_rs = tens_rs.cuda()
+            output = model(tens_rs_l)
+            loss = loss_fuction(tens_rs_ab, output)
+            loss.backward()
+            optimizer.step()
